fb1146284d28a5823f60dbaf942eeddfca7cdbb7
cushonz <cushonz@cwu.edu>
2022-04-26 19:55:53 -0700

    Nate files

---
+++ individual_Work/Nate/WeeklyScheduleView1.js
@@ -0,0 +1,593 @@
+import React from "react";
+import styled from "styled-components";
+export const WeeklyScheduleView1 = ({}) => {
+  return (
+    <WeeklyScheduleViewRoot>
+      <Union>
+        <SidebarBackground>
+          <ScheduleBackground>
+            <ScheduleIcon
+              src={"https://file.rendit.io/n/xY4D72KBcLuonsvE579A.svg"}
+            />
+            <Text1>Schedule</Text1>
+          </ScheduleBackground>
+          <EmployeesBackground>
+            <EmployeesIcon
+              src={"https://file.rendit.io/n/9rHcaPVk3cu9H1Yjg6Wi.svg"}
+            />
+            <Text2>Employees</Text2>
+          </EmployeesBackground>
+          <WhiteFlexRow>
+            <FreshProduceIcon
+              src={"https://file.rendit.io/n/eXH2EB7D6yGrLI8h7prT.png"}
+            />
+            <Text3>Fresh Produce</Text3>
+          </WhiteFlexRow>
+          <FlexRow>
+            <BusinessInfoIcon
+              src={"https://file.rendit.io/n/d0xEkKVCkVJQb3yyB0LF.svg"}
+            />
+            <Text3>Business Info</Text3>
+          </FlexRow>
+          <SettingsBackground>
+            <SettingsIcon
+              src={"https://file.rendit.io/n/t8iVdWcSBmFDXNzMi5q2.svg"}
+            />
+            <Text3>Settings</Text3>
+          </SettingsBackground>
+        </SidebarBackground>
+        <FlexColumn>
+          <Background55>
+            <Text6>This Week’s Schedule</Text6>
+            <Element20>
+              <Element19>4/18 - 4/24 </Element19>
+              <ArrowRight
+                src={"https://file.rendit.io/n/GOuY4EN2nmNNZxG0LPWL.svg"}
+              />
+              <ArrowLeft
+                src={"https://file.rendit.io/n/HWe5s5twi9Wx9VVGiQPQ.svg"}
+              />
+            </Element20>
+          </Background55>
+          <WeekDaySelector>
+            <WhiteText backgroundColor={"#fff8e8"}>Week</WhiteText>
+            <WhiteText backgroundColor={"#ffffff"}>Day</WhiteText>
+          </WeekDaySelector>
+          <WeeklyCalendar>
+            <FlexColumn1>
+              <Background15>
+                <Text7 width={"58px"}>Nate</Text7>
+              </Background15>
+              <Background15>
+                <Text7 width={"58px"}>Abdi</Text7>
+              </Background15>
+              <Background15>
+                <Text7 width={"89px"}>Garrett</Text7>
+              </Background15>
+              <Background15>
+                <Text7 width={"71px"}>
+                  Adam
+                  <br />
+                </Text7>
+              </Background15>
+              <Background15>
+                <Text7 width={"77px"}>Manni</Text7>
+              </Background15>
+              <Background15>
+                <Text7 width={"58px"}>
+                  Zach
+                  <br />
+                </Text7>
+              </Background15>
+            </FlexColumn1>
+            <FlexColumn2 width={"177px"}>
+              <Mon>
+                Mon
+                <br />
+                4/18
+              </Mon>
+              <WhiteText2>10:00 - 3:00</WhiteText2>
+              <Background margin={"0px 0px 17px 0px"} />
+              <WhiteText3 padding={"0px 20px"}>5:00- Close</WhiteText3>
+              <Background margin={"0px 0px 17px 0px"} />
+              <Background margin={"0px 0px 17px 0px"} />
+              <WhiteText4 margin={"0"}>4:00- 8:00</WhiteText4>
+            </FlexColumn2>
+            <FlexColumn2 width={"177px"}>
+              <Mon>
+                Tue
+                <br />
+                4/19
+              </Mon>
+              <WhiteText5 padding={"49px 15px 57px 18px"}>
+                10:00 - 3:00
+              </WhiteText5>
+              <WhiteText3 padding={"0px 20px"}>5:00- Close</WhiteText3>
+              <Background margin={"0px 0px 17px 0px"} />
+              <Background margin={"0px 0px 17px 0px"} />
+              <WhiteText4 margin={"0px 0px 17px 0px"}>4:00- 8:00</WhiteText4>
+              <Background margin={"0"} />
+            </FlexColumn2>
+            <FlexColumn2 width={"178px"}>
+              <Mon>
+                Wed
+                <br />
+                4/20
+              </Mon>
+              <WhiteText5 padding={"49px 10px 57px 24px"}>
+                10:00 - 3:00
+              </WhiteText5>
+              <Background2 margin={"0px 0px 17px 0px"} />
+              <WhiteText3 padding={"0px 20px 0px 21px"}>5:00- Close</WhiteText3>
+              <WhiteText10>4:00- 8:00</WhiteText10>
+              <Background2 margin={"0px 0px 17px 0px"} />
+              <Background2 margin={"0"} />
+            </FlexColumn2>
+            <FlexColumn2 width={"177px"}>
+              <Mon>
+                Thur
+                <br />
+                4/21
+              </Mon>
+              <WhiteText5 padding={"49px 17px 57px 16px"}>
+                10:00 - 3:00
+              </WhiteText5>
+              <WhiteText4 margin={"0px 0px 17px 0px"}>4:00- 8:00</WhiteText4>
+              <Background margin={"0px 0px 17px 0px"} />
+              <WhiteText3 padding={"0px 20px"}>5:00- Close</WhiteText3>
+              <Background margin={"0px 0px 17px 0px"} />
+              <WhiteText14 margin={"0"}>4:30- 8:30</WhiteText14>
+            </FlexColumn2>
+            <FlexColumn2 width={"177px"}>
+              <Mon>
+                Fri
+                <br />
+                4/22
+              </Mon>
+              <WhiteText5 padding={"49px 10px 57px 23px"}>
+                10:00 - 3:00
+              </WhiteText5>
+              <WhiteText14 margin={"0px 0px 17px 0px"}>4:30- 8:30</WhiteText14>
+              <WhiteText14 margin={"0px 0px 17px 0px"}>4:30- 8:30</WhiteText14>
+              <WhiteText4 margin={"0px 0px 17px 0px"}>4:00- 8:00</WhiteText4>
+              <WhiteText3 padding={"0px 20px"}>5:00- Close</WhiteText3>
+              <Background margin={"0"} />
+            </FlexColumn2>
+            <FlexColumn2 width={"177px"}>
+              <Mon>
+                Sat
+                <br />
+                4/23
+              </Mon>
+              <Background margin={"0px 0px 17px 0px"} />
+              <WhiteText20 margin={"0px 0px 17px 0px"}>On Call</WhiteText20>
+              <WhiteText14 margin={"0px 0px 17px 0px"}>4:30- 8:30</WhiteText14>
+              <WhiteText4 margin={"0px 0px 17px 0px"}>4:00- 8:00</WhiteText4>
+              <WhiteText14 margin={"0px 0px 17px 0px"}>4:30- 8:30</WhiteText14>
+              <WhiteText24>5:00- Close</WhiteText24>
+            </FlexColumn2>
+            <FlexColumn8>
+              <Mon>
+                Sun
+                <br />
+                4/24
+              </Mon>
+              <Background margin={"0px 0px 17px 0px"} />
+              <WhiteText14 margin={"0px 0px 17px 0px"}>4:30- 8:30</WhiteText14>
+              <WhiteText4 margin={"0px 0px 17px 0px"}>4:00- 8:00</WhiteText4>
+              <WhiteText3 padding={"0px 20px"}>5:00- Close</WhiteText3>
+              <Background margin={"0px 0px 17px 0px"} />
+              <WhiteText20 margin={"0"}>On Call</WhiteText20>
+            </FlexColumn8>
+          </WeeklyCalendar>
+        </FlexColumn>
+      </Union>
+    </WeeklyScheduleViewRoot>
+  );
+};
+const WeeklyScheduleViewRoot = styled.div`
+  background-color: #ffffff;
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  justify-content: center;
+  margin: auto;
+  min-width: 1920px;
+  align-items: center;
+`;
+const Union = styled.div`
+  width: 1920px;
+  background-image: url("https://file.rendit.io/n/dsDBJriBLZgTRGKRtBvy.svg");
+  background-size: cover;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 30px;
+  align-items: center;
+`;
+const SidebarBackground = styled.div`
+  height: 1376px;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  border-radius: 2px;
+  padding: 202px 0px;
+`;
+const ScheduleBackground = styled.div`
+  width: 191px;
+  background-color: #000000;
+  display: flex;
+  align-self: flex-end;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 44px;
+  align-items: center;
+  border-radius: 18px;
+  padding: 21px 27px;
+  margin: 0px 40px 15px 0px;
+`;
+const ScheduleIcon = styled.img`
+  width: 26px;
+  height: 28px;
+`;
+const Text1 = styled.div`
+  font-size: 18px;
+  font-family: Actor;
+  font-weight: 400;
+  color: #ffffff;
+`;
+const EmployeesBackground = styled.div`
+  width: 195px;
+  background-color: #ffffff;
+  display: flex;
+  align-self: flex-end;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 33px;
+  align-items: center;
+  border-radius: 18px;
+  padding: 25px 25px 24px 25px;
+  margin: 0px 40px 10px 0px;
+`;
+const EmployeesIcon = styled.img`
+  width: 31px;
+  height: 31px;
+`;
+const Text2 = styled.div`
+  font-size: 18px;
+  font-family: Actor;
+  font-weight: 400;
+  align-self: flex-start;
+  margin: 4px 0px 0px 0px;
+`;
+const WhiteFlexRow = styled.div`
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+  align-items: center;
+  padding: 18px 100px 18px 78px;
+  margin: 0px 0px 10px 0px;
+`;
+const FreshProduceIcon = styled.img`
+  width: 44px;
+  height: 44px;
+`;
+const FlexRow = styled.div`
+  background-image: url("https://file.rendit.io/n/XpRhrKlAgPmN8bp7v1eV.svg");
+  background-size: cover;
+  display: flex;
+  flex-direction: row;
+  gap: 23px;
+  align-items: center;
+  padding: 21px 103px 27px 82px;
+  margin: 0px 0px 10px 0px;
+`;
+const BusinessInfoIcon = styled.img`
+  width: 36px;
+  height: 32px;
+`;
+const SettingsBackground = styled.div`
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: row;
+  gap: 44.46px;
+  align-items: center;
+  padding: 21px 122px 27px 84.44px;
+`;
+const SettingsIcon = styled.img`
+  width: 31.1px;
+  height: 32px;
+`;
+const FlexColumn = styled.div`
+  width: 1507px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 35px;
+  flex-grow: 1;
+  align-items: center;
+  padding: 57px 0px;
+`;
+const Background55 = styled.div`
+  box-shadow: 0px 16px 31px 0px rgba(0, 0, 0, 0.01);
+  height: 119px;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  gap: 2px;
+  align-items: center;
+  border-radius: 34px;
+  padding: 24px 126px;
+`;
+const Text6 = styled.div`
+  font-size: 28px;
+  font-family: Actor;
+  font-weight: 400;
+`;
+const Element20 = styled.div`
+  height: 58px;
+  position: relative;
+  min-width: 264px;
+  align-items: center;
+`;
+const Element19 = styled.div`
+  display: flex;
+  font-size: 28px;
+  font-family: Actor;
+  font-weight: 400;
+  width: 264px;
+  height: 34px;
+  background-color: #fabb18;
+  position: absolute;
+  top: 4px;
+  flex-direction: row;
+  justify-content: center;
+  border-radius: 24px;
+  padding: 7px 0px 8px 0px;
+  white-space: pre-wrap;
+`;
+const ArrowRight = styled.img`
+  width: 50px;
+  height: 58px;
+  position: absolute;
+  left: 209px;
+`;
+const ArrowLeft = styled.img`
+  width: 54px;
+  height: 58px;
+  position: absolute;
+`;
+const WeekDaySelector = styled.div`
+  display: flex;
+  align-self: flex-end;
+  flex-direction: row;
+  gap: 30px;
+  justify-content: center;
+  align-items: center;
+  margin: 0px 50px 0px 0px;
+`;
+const WeeklyCalendar = styled.div`
+  width: 1507px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+const FlexColumn1 = styled.div`
+  width: 148px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  gap: 70px;
+  flex-grow: 1;
+  align-items: center;
+  padding: 26px 0px;
+  margin: 0px 29px 0px 0px;
+`;
+const WhiteText2 = styled.div`
+  display: flex;
+  font-size: 28px;
+  font-family: Actor;
+  font-weight: 400;
+  box-shadow: 0px 16px 31px 0px rgba(0, 0, 0, 0.01);
+  width: 144px;
+  height: 140px;
+  background-color: #ffffff;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 34px;
+  padding: 0px 17px 0px 16px;
+  margin: 0px 0px 17px 0px;
+`;
+const WhiteText10 = styled.div`
+  display: flex;
+  font-size: 28px;
+  font-family: Actor;
+  font-weight: 400;
+  box-shadow: 0px 16px 31px 0px rgba(0, 0, 0, 0.01);
+  width: 131px;
+  height: 140px;
+  background-color: #ffffff;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 34px;
+  padding: 0px 23px 0px 24px;
+  margin: 0px 0px 17px 0px;
+`;
+const WhiteText24 = styled.div`
+  display: flex;
+  font-size: 28px;
+  font-family: Actor;
+  font-weight: 400;
+  box-shadow: 0px 16px 31px 0px rgba(0, 0, 0, 0.01);
+  width: 137px;
+  height: 140px;
+  background-color: #ffffff;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 34px;
+  padding: 0px 20px;
+`;
+const FlexColumn8 = styled.div`
+  width: 177px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex-grow: 1;
+  align-items: center;
+`;
+const Text3 = styled.div`
+  font-size: 18px;
+  font-family: Actor;
+  font-weight: 400;
+`;
+const WhiteText = styled.div`
+  display: flex;
+  font-size: 28px;
+  font-family: Actor;
+  font-weight: 400;
+  border-width: 1px;
+  border-style: solid;
+  width: 234px;
+  height: 34px;
+  flex-direction: row;
+  justify-content: center;
+  border-radius: 10px;
+  padding: 27px 0px;
+  background-color: ${(props) => props.backgroundColor};
+`;
+const Background15 = styled.div`
+  width: 148px;
+  background-color: #ffdd68;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  padding: 27px 0px 28px 0px;
+`;
+const Text7 = styled.div`
+  height: 32px;
+  font-size: 28px;
+  font-family: Actor;
+  font-weight: 400;
+  width: ${(props) => props.width};
+`;
+const FlexColumn2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex-grow: 1;
+  align-items: center;
+  margin: 0px 15px 0px 0px;
+  width: ${(props) => props.width};
+`;
+const Mon = styled.div`
+  text-align: center;
+  display: flex;
+  font-size: 28px;
+  font-family: Actor;
+  font-weight: 400;
+  color: #ffffff;
+  width: 148px;
+  height: 68px;
+  background-color: #000000;
+  flex-direction: row;
+  justify-content: center;
+  border-radius: 10px;
+  padding: 9px 0px 10px 0px;
+  margin: 0px 0px 14px 0px;
+`;
+const Background = styled.div`
+  box-shadow: 0px 16px 31px 0px rgba(0, 0, 0, 0.01);
+  width: 177px;
+  height: 140px;
+  background-color: #ffffff;
+  border-radius: 34px;
+  margin: ${(props) => props.margin};
+`;
+const WhiteText3 = styled.div`
+  display: flex;
+  font-size: 28px;
+  font-family: Actor;
+  font-weight: 400;
+  box-shadow: 0px 16px 31px 0px rgba(0, 0, 0, 0.01);
+  width: 137px;
+  height: 140px;
+  background-color: #ffffff;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 34px;
+  margin: 0px 0px 17px 0px;
+  padding: ${(props) => props.padding};
+`;
+const WhiteText4 = styled.div`
+  display: flex;
+  font-size: 28px;
+  font-family: Actor;
+  font-weight: 400;
+  box-shadow: 0px 16px 31px 0px rgba(0, 0, 0, 0.01);
+  width: 131px;
+  height: 140px;
+  background-color: #ffffff;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 34px;
+  padding: 0px 23px;
+  margin: ${(props) => props.margin};
+`;
+const WhiteText5 = styled.div`
+  display: flex;
+  font-size: 28px;
+  font-family: Actor;
+  font-weight: 400;
+  box-shadow: 0px 16px 31px 0px rgba(0, 0, 0, 0.01);
+  width: 144px;
+  height: 34px;
+  background-color: #ffffff;
+  flex-direction: column;
+  border-radius: 34px;
+  margin: 0px 0px 17px 0px;
+  padding: ${(props) => props.padding};
+`;
+const Background2 = styled.div`
+  box-shadow: 0px 16px 31px 0px rgba(0, 0, 0, 0.01);
+  width: 178px;
+  height: 140px;
+  background-color: #ffffff;
+  border-radius: 34px;
+  margin: ${(props) => props.margin};
+`;
+const WhiteText14 = styled.div`
+  display: flex;
+  font-size: 28px;
+  font-family: Actor;
+  font-weight: 400;
+  box-shadow: 0px 16px 31px 0px rgba(0, 0, 0, 0.01);
+  width: 122px;
+  height: 140px;
+  background-color: #ffffff;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 34px;
+  padding: 0px 28px 0px 27px;
+  margin: ${(props) => props.margin};
+`;
+const WhiteText20 = styled.div`
+  display: flex;
+  font-size: 28px;
+  font-family: Actor;
+  font-weight: 400;
+  box-shadow: 0px 16px 31px 0px rgba(0, 0, 0, 0.01);
+  width: 87px;
+  height: 140px;
+  background-color: #ffffff;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 34px;
+  padding: 0px 45px;
+  margin: ${(props) => props.margin};
+`;
