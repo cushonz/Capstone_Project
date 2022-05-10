7a6235c1ae9d90cd2602e8d15ba16325f4643293
Nate Goodman <nategoodman4241@gmail.com>
2022-05-09 20:35:32 -0700

    Sign up page and other updated files

--- individual_Work/Nate_Abdi/App.js
+++ individual_Work/Nate_Abdi/App.js
@@ -1,8 +1,8 @@
 
-    import {WeeklyScheduleView1} from "./WeeklyScheduleView1";
+    import {SignUpPage1} from "./SignUpPage1";
 
     export default function App() {
         document.body.style.backgroundColor = '#e5e5e5';
         document.body.style.margin = '0';
-        return <WeeklyScheduleView1 />;
+        return <SignUpPage1 />;
     }
