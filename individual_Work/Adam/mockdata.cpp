f70188e9339e59529d2e4146a13e397d15e3c394
KuraiSol <46945261+KuraiSol@users.noreply.github.com>
2022-05-10 21:13:16 -0700

    weights work

--- individual_Work/Adam/mockdata.cpp
+++ individual_Work/Adam/mockdata.cpp
@@ -6,7 +6,7 @@ int main (){
     
     std::ofstream file = std::ofstream("mockdata.csv");
     int average = 5;
-    float monthMod[12] = {.2,.8,.7,.6,.5,
+    float monthMod[12] = {.4,.8,.7,.6,.5,
                           .5,.6,.5,.6,.7,
                            1,.9};
     float monthDays[12] = {31,28,31,30,31,
@@ -14,12 +14,12 @@ int main (){
                            30,31};
     float yearMod[3] = {.6, .8, 1};
 
-    float weekdayMod[7] = {0, .3, .5, .8, 1, .8, 0};
+    float weekdayMod[7] = {0, .4, .5, .8, 1, .8, 0};
     int values = 0;
     for (int k = 0; k < 3; k++){
         for (int i = 0; i < 12; i++){
             for (int j = 0; j < monthDays[i]; j++) {
-                file << std::round((4+rand()%4)*(monthMod[i]+weekdayMod[values%7]+yearMod[k])/3);
+                file << std::round((4+rand()%4) * monthMod[i] * weekdayMod[values%7] * yearMod[k]);
                 file << ',';
                 if (values % 7 == 0 && values > 0){
                     file << std::endl;
