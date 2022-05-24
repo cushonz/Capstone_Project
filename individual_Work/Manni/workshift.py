5752df6c5d29c196b5c394773865982d324b5b42
Manjinder Singh <97862048+Manni000@users.noreply.github.com>
2022-05-24 12:17:23 -0700

    Update workshift.py

--- individual_Work/Manni/workshift.py
+++ individual_Work/Manni/workshift.py
@@ -10,6 +10,17 @@ class workshift:
         self._employee = employee
         self._hours = start-end
 
+    def __str__(self):
+        # string representation of a 'workshift' object
+        return f'''
+        Today: {self._day},
+        Employee Name: {self._employee.NAME()},
+        Employee Role: {self._role},
+        Start Time: {self._start},
+        End Time: {self._end},
+        Total Hours for Today: {self._hours}
+        '''
+
     def checkConflict(self, employee:Employee) -> bool:
         if (employee._role == self._role &
             employee.availabilities[self.day,0] >= self.start & 
